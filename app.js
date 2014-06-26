c9c05f88a386c9c1ca8121e97915abd2cc5c0100
Hannes Lemberg <lemberg@silpion.de>
2014-06-26 16:13:14 +0200

    simple test page without navigation

---
+++ app.js
@@ -0,0 +1,7 @@
+(function (angular) {
+    var module = angular.module('Main', []);
+    module.controller("MainController", function () {
+        this.text = "HalloWelt";
+    });
+})(window.angular);
+
