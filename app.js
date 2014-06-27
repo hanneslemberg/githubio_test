98959cbd699ac824b4b54df360d81ea573f9124d
Hannes Lemberg <lemberg@silpion.de>
2014-06-27 10:36:28 +0200

    changed to company page with navigation

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 (function (angular) {
-    var module = angular.module('Main', []);
+    var module = angular.module('Main', ['ngRoute']);
+    module.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {templateUrl: 'partials/home.html'})
+            .when('/about', {templateUrl: 'partials/imprint.html'})
+            .when('/where', {templateUrl: 'partials/where.html'})
+            .otherwise({redirectTo: '/'});
+    }]);
     module.controller("MainController", function () {
-        this.text = "HalloWelt. Included via normal binding";
+        this.text = "Willkommen auf der Silenamenti GmbH Testseite. Diese Seite ist eine AngularJS Testseite";
     });
 })(window.angular);
 
